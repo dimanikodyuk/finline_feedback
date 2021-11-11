aa3c95d16339cd867d8036e03cc9c7d8c9f52a19
nykodiuk.d <dimanikodyuk@gmail.com>
2021-11-11 15:32:14 +0200

    Added exception to main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from db.models import DeltaDB
 from db.config import partner_conf
 from resourses.requests import send_request
+from logs.logs import logger_finline
 
 if __name__ == "__main__":
-    res = DeltaDB().finline_feedback()
+    try:
+        res = DeltaDB().finline_feedback()
 
-    for row in res:
-        send_request(row['lead_id'], row['status_name'], partner_conf['api_key'], partner_conf['partner_url'])
+        for row in res:
+            send_request(row['lead_id'], row['status_name'], partner_conf['api_key'], partner_conf['partner_url'])
 
+    except ValueError as err:
+        logger_finline.error("Помилка даних main.py: " + str(err))
+    except Exception as err:
+        logger_finline.error("Помилка: " + str(err))
